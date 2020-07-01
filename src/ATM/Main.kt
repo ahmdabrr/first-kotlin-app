d7bc5bb57f39994e69be2ec8965f76bd7b2b659d
wisungyo <wisungyo@gmail.com>
2020-07-01 20:35:42 +0700

    finishing

--- src/ATM/Main.kt
+++ src/ATM/Main.kt
@@ -1,9 +1,24 @@
 package ATM
 
 fun main(args: Array<String>) {
+    intro("BCA")
     print("Enter your name : ")
     val name: String = readLine().toString()
     val atmObject: ATM = ATM(name)
     atmObject.run()
     val bankObject: Bank = Bank(name, "Thank You")
+    intro("BCA", "BYE")
+}
+
+fun intro(bank: String){
+    println("========================")
+    println("==      BANK $bank      ==")
+    println("========================")
+}
+
+fun intro(bank: String, message: String){
+    println("========================")
+    println("==      BANK $bank      ==")
+    println("==        $message         ==")
+    println("========================")
 }
